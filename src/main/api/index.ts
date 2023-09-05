e84ebae6efaefdad564f3f92514caf21e91174f1
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-09-05 08:57:41 -0300

    set strict mode

--- src/main/api/index.ts
+++
@@ -1,6 +0,0 @@
-import '../config/module-alias'
-import { PersonController } from "@/application/controllers"
-
-const p = new PersonController()
-console.log(p.speak('Rodrigo'))
-console.log(p.speak())
